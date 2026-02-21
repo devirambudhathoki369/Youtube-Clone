ae80ed5312826049ce1ca4d9742a58cb8804c08e
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-02-21 16:45:53 +0545

    Views for user creation added

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth import login
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -7,3 +8,15 @@ from .forms import CustomUserCreationForm
 # Create your views here.   
 class RegisterView(CreateView):
     form_class= CustomUserCreationForm
+    success_url = reverse_lazy('accounts:login')
+    template_name = 'accounts/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return redirect('/')
